d7135f1ca97e4cb8774b7fa238f56d85ef203c34
murk0s <m.kamneva@gmail.com>
2025-04-17 10:02:47 +0400

    основные отлия от main: 1. Закомментировала класс консюмера 2. изменен докер-компос, изменены настройки кафки в application.properties 3. в контроллере на пост метод добавлена тестовая отправка сообщения 4. в продюсере поправлено свойство для bootstrapAddress

--- src/main/java/sia/pairschallenge/kafka/KafkaProducerConfig.java
+++ src/main/java/sia/pairschallenge/kafka/KafkaProducerConfig.java
@@ -12,30 +12,30 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.Map;
 
-//@Configuration
+@Configuration
 public class KafkaProducerConfig {
 
-//    @Value("${spring.kafka.producer.bootstrap-servers}")
-//    private String bootstrapAddress;
-//
-//    @Bean
-//    public ProducerFactory<String, String> producerFactory() {
-//        return new DefaultKafkaProducerFactory<>(producerConfigs());
-//    }
-//
-//    @Bean
-//    public KafkaTemplate<String, String> kafkaTemplate() {
-//        return new KafkaTemplate<>(producerFactory());
-//    }
-//
-//    private Map<String, Object> producerConfigs() {
-//        return Map.of(
-//                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-//                bootstrapAddress,
-//                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-//                StringSerializer.class,
-//                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-//                JsonSerializer.class
-//        );
-//    }
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    private Map<String, Object> producerConfigs() {
+        return Map.of(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapAddress,
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                JsonSerializer.class
+        );
+    }
 }
